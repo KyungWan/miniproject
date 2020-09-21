5d5d573e2d96b94434d7678f7593a03324d51e34
KyungWan <wanni26>
2020-09-21 17:58:42 +0900

    Project

--- frontend/src/views/CrawlCategory.vue
+++ frontend/src/views/CrawlCategory.vue
@@ -47,7 +47,7 @@
               <input type="text" name="keyword" placeholder="검색" />
               <input type="hidden" name="pageNum" placeholder="pageNum">
               <input type="hidden" name="amount" placeholder="amount">
-              <button class="btn btn-primary btn-sm">Search</button>
+              <button class="btn btn-primary btn-sm"><p style="border-style: groove">Search</p></button>
             </form>
           </div>
         </div>
@@ -106,38 +106,44 @@ export default {
 </script>
 
 <style>
-body {
-  font-family: Helvetica Neue, Arial, sans-serif;
-  font-size: 14px;
-  color: #444;
-}
+  .border-styles > p{
+    margin: 2px 0;
+    padding: 1px 3px;
+    border-width: 2px;
+    border-color: #aaa;
+  }
+  body {
+    font-family: Helvetica Neue, Arial, sans-serif;
+    font-size: 14px;
+    color: #444;
+  }
 
-table {
-  border: 2px solid #42b983;
-  border-radius: 3px;
-  background-color: #fff;
-}
+  table {
+    border: 2px solid #42b983;
+    border-radius: 3px;
+    background-color: #fff;
+  }
 
-th {
-  background-color: #42b983;
-  color: rgba(255, 255, 255, 0.66);
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -user-select: none;
-}
+  th {
+    background-color: #42b983;
+    color: rgba(255, 255, 255, 0.66);
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -user-select: none;
+  }
 
-td {
-  background-color: #f9f9f9;
-}
+  td {
+    background-color: #f9f9f9;
+  }
 
-th,
-td {
-  min-width: 120px;
-  padding: 10px 20px;
-}
+  th,
+  td {
+    min-width: 120px;
+    padding: 10px 20px;
+  }
 
-#search {
-  margin-bottom: 10px;
-}
+  #search {
+    margin-bottom: 10px;
+  }
 </style>
