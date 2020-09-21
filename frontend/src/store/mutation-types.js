e38a09da6580aa0ca9ba013a1e8638fb65cd5c82
KyungWan <wanni26>
2020-09-21 18:46:13 +0900

    Project

--- frontend/src/store/mutation-types.js
+++ frontend/src/store/mutation-types.js
@@ -10,3 +10,7 @@ export const CRAWLSTART = 'CRAWLSTART'
 export const FINDONE = 'FINDONE'
 export const FINDHOME = 'FINDHOME'
 export const BOOLEAN = 'BOOLEAN'
+
+// FETCH_BOARD
+export const FETCH_BOARD_LIST = 'FETCH_BOARD_LIST'
+export const FETCH_BOARD = 'FETCH_BOARD'
